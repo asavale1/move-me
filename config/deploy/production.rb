c9f26f0fbc85df586c9b9c8b6ed138a6cf460cf1
Ameya <asavale1@yahoo.com>
2014-12-09 19:51:45 -0800

    setting rails env during cap deploy

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -15,6 +15,8 @@
 # server list. The second argument is a, or duck-types, Hash and is
 # used to set extended properties on the server.
 
+set :rails_env, "production"
+
 server 'confederation', user: 'ameya', roles: [:app, :web, :db]
 
 
